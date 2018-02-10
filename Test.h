3d11af2c02146b241ba2f3914a10f725f0889b1f
Joseph Gerardot <indyJAG@gmail.com>
2018-02-10 17:59:13 -0500

    Create `Test.h` with declarations for `Test` struct. Add `.gitignore`

---
+++ Test.h
@@ -0,0 +1,18 @@
+#ifndef _CRUMTEST_
+/* Test is a struct containing the information related to a single unit test in the `crumtest` program. It simply has two strings, the input and expected output.
+ * The strings will be dynamically allocated, so they need to be deleted when the struct is done. `destroyTest` and `ndestroyTest` are provided for this purpose. */
+
+struct Test {
+    char *input; // input to be fed into the tested program
+    char *output; // expected output of the program
+};
+/* `gentests` is a function to read through file named by the argument and produce an array of `Test`s represented the test cases defined in the file.
+ * Test definition syntax is defined in the README */
+Test *genTests(char *file, int *count);
+/* `destroyTest` deallocates a `Test`, pointed to by the argument */
+void destroyTest(Test *victim);
+/* `ndestroyTest` destroys n `Test`s, in an array pointed to by the first arg */
+void ndestroyTest(Test *victims, int n);
+
+#define _CRUMTEST_
+#endif
