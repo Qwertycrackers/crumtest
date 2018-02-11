fd11e9e7af028e46d910ccf9d214d5c8d8c38cdf
Joseph Gerardot <indyJAG@gmail.com>
2018-02-11 11:01:49 -0500

    Fix signature issues and typos in `Test.h`. Finish main.c.

--- Test.h
+++ Test.h
@@ -8,7 +8,7 @@ struct Test {
     char *input; // input to be fed into the tested program
     char *output; // expected output of the program
 };
-/* `gentests` is a function to read through file named by the argument and produce an array of `Test`s represented the test cases defined in the file.
+/* `gentests` is a function to read through file named by the argument and produce an array of `Test`s representing the test cases defined in the file.
  * Test definition syntax is defined in the README */
 int genTests(const char *file, struct Test **tests, int *count);
 /* `destroyTest` deallocates a `Test`, pointed to by the argument */
--- main.c
+++ main.c
@@ -5,27 +5,46 @@
  *
  * Author: Joseph Gerardot   */
 #include <stdio.h>
+#include <stdlib.h>
 #include "Test.h"
 
 int main(int argc, char **argv) { // we're going to accept command-line args
-    TestCase *cases; // a dynamic array of the test cases
-    int numCases; // the number of cases
-    int i = 0; // interator over the test cases
-    char *testFile; // the name of the file containing tests. Given from argv
-    char *testExec; // the name of the executable to test. Given from argv
-    if(argc < 3) {
-        printUsage();
-        return 0;
-    } // tell the user what to do, if they gave wrong number of arguments.
-    testExec = argv[1]; // the second element (first argument) should be the executable.
-    testFile = argv[2]; // the third element (second argument) should be the test file.
+  TestCase *cases; // a dynamic array of the test cases
+  int numCases; // the number of cases
+  int i = 0; // interator over the test cases
+  int status = 0; // tracker for success
+  char *testFile; // the name of the file containing tests. Given from argv
+  char *testExec; // the name of the executable to test. Given from argv
+  char *error = NULL; // string to hold the dianostic errors
+  if(argc < 3) {
+    printUsage();
+    return 0;
+  } // tell the user what to do, if they gave wrong number of arguments.
+  testExec = argv[1]; // the second element (first argument) should be the executable.
+  testFile = argv[2]; // the third element (second argument) should be the test file.
 
-    if(genTestCases(testFile, &cases, &numCases) != 0) {
-        puts("Error encountered when generating test cases! Aborting...");
-        return -1;
-    } // generate the test cases, and complain if something goes wrong. `cases` should now be an array of TestCases.
-    while(i < numCases) {
-        switch(test(testExec,cases + i++)) { // switch on result of test, increment i along with pointer arithmetic
-            case 0:
-                printf("Test %d resolved successfully.\n", i+1);
-                break; 
+  if(genTestCases(testFile, &cases, &numCases) != 0) {
+    puts("Error encountered when generating test cases! Aborting...");
+    return -1;
+  } // generate the test cases, and complain if something goes wrong. `cases` should now be an array of TestCases.
+  while(i < numCases) {
+    switch(execTest(testExec,cases + i++, &error)) { // switch on result of test, increment i along with pointer arithmetic
+      case 0:
+        printf("Test %d resolved successfully.\n", i+1);
+        break; 
+      case -1:
+        printf("Test %d failed. Reason: \n%s", i + 1, error);
+        free(error);
+        error = NULL;
+        break;
+      case -2:
+        printf("crumtest itself encountered a problem. Error: \n%s", error);
+        status = -1;
+        free(error);
+        error = NULL;
+        break;
+      default:
+    }
+  }
+  return status;
+}
