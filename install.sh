ff9dd0e054690780e2dcea67b0a82b12782b5800
Joseph Gerardot <indyJAG@gmail.com>
2018-02-15 22:05:39 -0500

    Make install just put the file in CS159 folder

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.crumtest # create the crumtest folder, if it does not exist
+mkdir -p ~/CS159 # create the CS159 folder, though it will almost certainly exist
 gcc *.c -o crumtest # compile
-cp crumtest ~/.crumtest # place crumtest in its home
-
-echo "setenv PATH \$PATH:\$HOME/.crumtest" >> ~/.profile # make crumtest directory searchable in PATH
-source ~/.profile
-
+cp crumtest ~/CS159 # place crumtest in its home
