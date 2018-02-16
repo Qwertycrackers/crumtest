5b601e17ffefbebb7cce8a95756e0cbfc0a901a8
Joseph Gerardot <indyJAG@gmail.com>
2018-02-15 21:15:03 -0500

    Add an install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p ~/.crumtest # create the crumtest folder, if it does not exist
+gcc *.c -o crumtest # compile
+cp crumtest ~/.crumtest # place crumtest in its home
+
+echo "export PATH=$PATH:~/.crumtest" >> ~/.profile # make crumtest directory searchable in PATH
+source ~/.profile
+
